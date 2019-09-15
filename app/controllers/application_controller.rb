f20bdab3307ae77c289372e0987d1370a6049a9a
hibiking <hibi0422@gmail.com>
2019-09-15 01:16:30 +0000

    ログイン前整頓完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        projects_url
+      end
+      
+      def after_sign_out_path_for(resource)
+        new_user_session_url
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
   devise_for :users
-  get 'projects/index'
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  resources :projects
 
-  root 'projects#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'home#index'
+
 end
