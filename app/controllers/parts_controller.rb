8f164dc618a2ce74641b242d5a2fc6bb6505cee8
hibiking <hibi0422@gmail.com>
2019-09-17 21:20:17 +0000

    表完成

--- app/controllers/parts_controller.rb
+++ app/controllers/parts_controller.rb
@@ -2,20 +2,34 @@ class PartsController < ApplicationController
   def index
     @project = Project.find(params[:project_id])
     @parts = @project.part
+
+    if params[:count].present?
+      @count = params[:count]
+    end
   end
 
   def new
-    @part = Part.new
   end
 
   def create
-    part = Part.new(part_params)
-
-    if part.save
-      redirect_to parts_path(project_id: params[:part][:project_id])
-    else
-      render 'new'
+    count = 0
+    10.times do |i|
+      i *= 6
+    if params[:parts][i].present? && params[:parts][i+1].present? && params[:parts][i+2].present? && params[:parts][i+3].present? && params[:parts][i+4].present? && params[:parts][i+5].present?
+      part = Part.new(
+        part_name: params[:parts][i],
+        maker: params[:parts][i+1],
+        vender: params[:parts][i+2],
+        unit_price: params[:parts][i+3],
+       volume: params[:parts][i+4],
+       day: params[:parts][i+5],
+       project_id: params[:project_id]
+      )
+      part.save!
+      count += 1
+     end
     end
+    redirect_to parts_path(project_id: params[:project_id],count: count)
   end
 
   def edit
