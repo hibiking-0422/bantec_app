357a3ee688457ab76c983cf18a4eb70eeac133f6
vagrant <vagrant@localhost.localdomain>
2019-09-22 03:53:25 +0000

    いいかんじ

---
+++ db/migrate/20190921191252_remove_wage_to_project.rb
@@ -0,0 +1,4 @@
+class RemoveWageToProject < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_21_091733) do
+ActiveRecord::Schema.define(version: 2019_09_21_191252) do
 
   create_table "drivers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "driver_cost"
